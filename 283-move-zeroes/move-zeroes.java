a9645211d3823cb03474b0b07686f1fa03cc34a3
Meghana Golla <gollameghana33@gmail.com>
2025-08-13 23:50:28 +0530

    Time: 3 ms (21.41%) | Memory: 46.9 MB (6.80%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int count=0;
+        for(int i=0; i<=arr.length-1; i++){
+            if(arr[i]!=0){
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count]=temp;
+                count++;
+            }
+        }
+    }
+}
