cda210ddd03115312e83de026a00b00ca77f0f5f
Meghana Golla <gollameghana33@gmail.com>
2025-08-20 17:56:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (9.39%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] plusOne(int[] arr) {
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i] <9){
+                arr[i]++;
+                return arr;
+            }
+            arr[i]=0;
+        }
+        int[] arr1= new int[arr.length+1];
+        arr1[0]=1;
+        return arr1;
+    }
+}
