2157bbfb22419b7547a84462e444b7622ea371fb
Meghana Golla <gollameghana33@gmail.com>
2025-09-09 23:59:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (97.53%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean isHappy(int n) {
         int sum = n, x = n;
-
-        // this loop executes till the sum of square of
-        // digits obtained is not a single digit number
         while (sum > 9) {
             sum = 0;
 
-            // this loop finds the sum of square of digits
             while (x > 0) {
                 int d = x % 10;
                 sum += d * d;
