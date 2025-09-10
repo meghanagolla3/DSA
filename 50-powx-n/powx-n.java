8926aab204fb53b0b7e97770ab6b985cbcb8219e
Meghana Golla <gollameghana33@gmail.com>
2025-09-10 12:32:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (68.51%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,26 +1,29 @@
 class Solution {
-    public double myPow(double x, int n) {
-        long N = n;
-
-        // Edge case: 0 raised to negative power is undefined
-        if (x == 0 && N < 0) {
-            return Double.POSITIVE_INFINITY; // or throw exception
+    public double pow(double x,long n)
+    {
+        if(n==0)
+        {
+            return 1.0;
         }
-
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        return power(x, N);
-    }
-
-    private double power(double x, long n) {
-        if (n == 0) return 1.0;
-        double half = power(x, n / 2);
+         double half = pow(x, n / 2);
         if (n % 2 == 0) {
             return half * half;
         } else {
             return half * half * x;
         }
     }
+    public double myPow(double x, int n) {
+        long N = n;
+        if (x == 0 && N < 0) {
+            return Double.POSITIVE_INFINITY; // or throw exception
+        }
+        if(n<0)
+        {
+            x = 1/x;
+            N =-N;
+        }
+            return pow(x,N);
+        
+    }
+
 }
