78fef8a78c1d2a6d999db3a6cd7335e214fd1acf
Meghana Golla <gollameghana33@gmail.com>
2025-09-11 21:28:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check K-th Bit/check-kth-bit.java
+++ Difficulty: Easy/Check K-th Bit/check-kth-bit.java
@@ -1,9 +1,7 @@
 class CheckBit {
     static boolean checkKthBit(int n, int k) {
         // code here
-        int bitMask = 1<<(k);
-        int ans = n & bitMask;
-        if(bitMask == ans ) return true;
-        else return false;
+        int bitMask = 1 << k;
+        return (n & bitMask) != 0;
     }
 }
