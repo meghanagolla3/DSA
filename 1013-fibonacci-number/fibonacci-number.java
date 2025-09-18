7e3a7a876a18d9419a491a9944a71d48dc8a04b4
Meghana Golla <gollameghana33@gmail.com>
2025-09-19 00:43:13 +0530

    Time: 8 ms (45.42%) | Memory: 40.1 MB (97.31%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -2,8 +2,7 @@ class Solution {
     public int fib(int n) {
         if(n <= 1){
             return n;
-        }else{
-            return fib(n-1) + fib(n-2);
         }
+         return fib(n-1) + fib(n-2);
     }
 }
